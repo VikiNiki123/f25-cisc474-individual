2d0df4c3f25d4a50e71d102850484c4894db3762
Victoria Nikolaeva <vniko@udel.edu>
2025-09-12 21:26:47 -0400

    Changing file names

--- apps/web/app/profile/page.tsx
+++ apps/web/app/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import styles from "./profile.module.css";
+import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Profile() {
